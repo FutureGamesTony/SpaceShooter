ffa2ad01682b3bd626a8323c43e6adddbc18cddf
CM\Tony.Rundstrom <tony.rundstrom@futuregames.nu>
2020-08-22 19:01:06 +0200

    Implemented steering and a bullet. The bullet spawns and destroy itself when out of bounds.

---
+++ SpaceShooter/Assets/Scripts/BulletScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+//Just here to destroy the bullet when it is out of bounds.
+public class BulletScript : MonoBehaviour
+{
+    private float _speed = 20f;
+    private float _bounds = 20f;
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x + (_speed * Time.deltaTime), transform.position.y);
+        if (transform.position.x > _bounds)
+            Destroy(gameObject);
+        
+    }
+}
--- SpaceShooter/Assets/Scripts/PlayerMovement.cs
+++ SpaceShooter/Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,47 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private Rigidbody2D _rb;
+    private string _horizontal = "Horizontal";
+    private string _vertical = "Vertical";
+    private float _moveVertical;
+    private float _moveHorizontal;
+    //TODO: Change from magic values to data collected from scene and camera
+    private float _speed = 15f;
+    private float _minX = -10f;
+    private float _maxX = 10f;
+    private float _minY = -4.5f;
+    private float _maxY = 4.5f;
 
-    // Update is called once per frame
+    public GameObject bullet;
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+
+    }
     void Update()
     {
-        
+        _moveVertical = Input.GetAxis("Vertical");
+        _moveHorizontal = Input.GetAxis("Horizontal");
+        transform.position = new Vector2(transform.position.x + (_moveHorizontal * _speed * Time.deltaTime), transform.position.y + (_moveVertical * _speed * Time.deltaTime));
+        OutOfBounds();
+        if (Input.GetButtonDown("Fire1") || Input.GetButtonDown("Fire4"))
+        {
+            Instantiate(bullet, new Vector3(transform.position.x, transform.position.y, 1), Quaternion.identity);
+        }
+    }
+    private void OutOfBounds()
+    {
+        if (transform.position.y > _maxY)
+            transform.position = new Vector2(transform.position.x, _maxY);
+       if (transform.position.y < _minY)
+            transform.position = new Vector2(transform.position.x, _minY);
+       if (transform.position.x < _minX)
+            transform.position = new Vector2(_minX, transform.position.y);
+       if (transform.position.x > _maxX)
+            transform.position = new Vector2(_maxX, transform.position.y);
     }
 }
