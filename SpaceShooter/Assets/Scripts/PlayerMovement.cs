275fbdfdf698d6c53d41bae892f8ead9ab44461c
sup3rsmash8 <justus617@hotmail.com>
2020-08-24 00:54:09 +0200

    Added 1 enemy, random object spawning, minor fixes
    Added one enemy type with behavior, shoots enemy bullets and is destructible;
    Health pack modified to scroll slowly to the left;
    Added object that randomly spawns enemies and health packs at the right of the screen;
    Player bounds now determined by Bounds.ClosestPoint to prevent moving out of bounds.

--- SpaceShooter/Assets/Scripts/PlayerMovement.cs
+++ SpaceShooter/Assets/Scripts/PlayerMovement.cs
@@ -1,27 +1,32 @@
-﻿
-using UnityEngine;
+﻿using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D _rb;
-    private string _horizontal = "Horizontal";
-    private string _vertical = "Vertical";
     private float _moveVertical;
     private float _moveHorizontal;
-    //TODO: Change from magic values to data collected from scene and camera
+    
     private float _speed = 15f;
-    private float _minX = -10f;
-    private float _maxX = 10f;
-    private float _minY = -4.5f;
-    private float _maxY = 4.5f;
+
+    private Bounds bounds;
+
+    //Basically 16:9, only when multiplied by orthographicSize it will become
+    //the original coordinates of the level.
+    Vector2 resolutionReference = new Vector2(3.2f, 1.8f);
 
     public GameObject bullet;
+
+    Camera mainCamera;
+    Canvas canvas;
+
     private void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
-
+        mainCamera = Camera.main;
+        bounds = new Bounds(mainCamera.transform.position, resolutionReference * mainCamera.orthographicSize);
     }
+
     void Update()
     {
         _moveVertical = Input.GetAxis("Vertical");
@@ -30,18 +35,22 @@ public class PlayerMovement : MonoBehaviour
         OutOfBounds();
         if (Input.GetButtonDown("Fire1") || Input.GetButtonDown("Fire4"))
         {
+            
             Instantiate(bullet, new Vector3(transform.position.x, transform.position.y, 1), Quaternion.identity);
         }
+
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
     }
+
     private void OutOfBounds()
     {
-        if (transform.position.y > _maxY)
-            transform.position = new Vector2(transform.position.x, _maxY);
-       if (transform.position.y < _minY)
-            transform.position = new Vector2(transform.position.x, _minY);
-       if (transform.position.x < _minX)
-            transform.position = new Vector2(_minX, transform.position.y);
-       if (transform.position.x > _maxX)
-            transform.position = new Vector2(_maxX, transform.position.y);
+        if (!bounds.Contains(transform.position))
+        {
+            Vector3 boundPosition = bounds.ClosestPoint(transform.position);
+            transform.position = new Vector2(boundPosition.x, boundPosition.y);
+        }
     }
 }
