76a84ad4e5cc9a25bc9b4de229c8e257f1635a46
sup3rsmash8 <justus617@hotmail.com>
2020-08-24 00:41:09 +0200

    Add files via upload
    Added one enemy type with behavior, shoots enemy bullets and is destructible; 
    Health pack modified to scroll slowly to the left; 
    Added object that randomly spawns enemies and health packs at the right of the screen; 
    Player bounds now determined by Bounds.ClosestPoint to prevent moving out of bounds.

---
+++ Assets/Scripts/RandomSpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSpawner : MonoBehaviour
+{
+    public GameObject[] pickupSpawns;
+    public GameObject[] enemySpawns;
+
+    public float pickupTimer = 20f;
+    public float enemyTimer = 7f;
+
+    public float yPositionRange = 4.5f;
+
+    Coroutine pickupSpawner;
+    Coroutine enemySpawner;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pickupSpawner = StartCoroutine(SpawnInterval(pickupSpawns, pickupTimer));
+        enemySpawner = StartCoroutine(SpawnInterval(enemySpawns, enemyTimer));
+    }
+
+    IEnumerator SpawnInterval(GameObject[] spawnableObjects, float timer)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(timer);
+            GameObject spawnedObject = Instantiate(spawnableObjects[Random.Range(0, spawnableObjects.Length)]);
+            spawnedObject.transform.position = new Vector2(transform.position.x, Random.Range(-yPositionRange, yPositionRange));
+        }
+    }
+}
